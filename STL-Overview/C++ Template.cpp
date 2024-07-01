d5b74d581d892be9a5b1771953a58d2f9dabe24e
Mohit Chaudhary <117006118+m0hitchaudhary@users.noreply.github.com>
2024-07-01 17:13:14 +0530

    Print the smallest of two inputs using the concept of Template

---
+++ STL-Overview/C++ Template.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template <class T>
+class minElement
+{
+    T y;
+    public:
+    minElement(T y1){
+        y = y1;
+    }
+    T check(T x){
+        T res = (y < x) ? y : x;
+        cout<<res<<"\n";
+    }
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int c;
+        cin>>c;
+        if(c==1)
+        {
+            string a,b;
+            cin>>a>>b;
+            minElement<string>obj1(a);
+            obj1.check(b);
+        }
+        else if(c==2)
+        {
+            int a,b;
+            cin>>a>>b;
+            minElement<int>obj2(a);
+            obj2.check(b);
+        }
+        else
+        {
+            char a,b;
+            cin>>a>>b;
+            minElement<char>obj3(a);
+            obj3.check(b);
+        }
+    }
+}
